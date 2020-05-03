d7d59fd435606adcef12569233184f4531eaae3b
divitha2468 <divithaelugoti@gmail.com>
2020-05-03 15:33:29 +0530

    java file

---
+++ hello.java
@@ -0,0 +1,11 @@
+import java.util;
+ public class Hello{
+     public static void main(String args[])
+    {
+       
+       int num=0xFFFFFFE;
+       System.out.printf("%d",num);
+       System.out.println("\n");
+       
+    }
+}
